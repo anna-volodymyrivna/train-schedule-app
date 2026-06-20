7b50148f274283645b9cdabefca7448fe8181236
Anna <anna.volodymyrivna19@gmail.com>
2026-06-20 14:18:40 +0300

    feat(users): manually implement users module with unique email check and roles

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TrainsModule } from './trains/trains.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, TrainsModule],
+  imports: [PrismaModule, TrainsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: {
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+  }) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    return this.prisma.user.create({ data });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+}
